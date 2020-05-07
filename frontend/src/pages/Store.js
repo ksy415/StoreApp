1e6fbd223a5a3e7bdc381c59aaf84104dba79efd
Kevin Sy <sykevin415@gmail.com>
2020-05-07 12:53:12 -0700

    Working on listing transactions

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -10,9 +10,31 @@ const Store = ({appUser, setAppUser, items}) => {
     const [currentUser, setCurrentUser] = React.useState('');
     const [cart, setCart] = React.useState([]);
     const [selectedItem, setSelectedItem] = React.useState('');
-    const[error, setError] = React.useState('');
+    const [transactionList, setTransactionList] = React.useState([]);
+    
+ 
+    // React.useEffect(() => {
+    //    // if(transactionList.length === 0) {
+           
+    //    // }
+    // });
 
+    const fetchTransactions = () => {
+
+        axios.get('/api/listTransactions')
+        .then((res) => {
+            setTransactionList(() => {
+                let myList = JSON.parse(res.data);
+                console.log(myList);
+                
+
+                return myList.transactionList;
+            });
+        });
+    };
+ 
    const itemSelector = (e) => {
+   // console.log(transactionList);
         const val = e.currentTarget.value;
         //console.log(appUser);
         setSelectedItem( () =>  val );
@@ -21,8 +43,9 @@ const Store = ({appUser, setAppUser, items}) => {
    const handleAddtoCart = () => {
 
         //console.log(`appUser = ${appUser}`);
+        
 
-        setCart( (cartItem) => {
+        setCart( () => {
             const newCart = cart.slice();
             newCart.push(selectedItem);
         
@@ -31,23 +54,32 @@ const Store = ({appUser, setAppUser, items}) => {
     }
 
     const handleSubmit = () => {
-
+        //console.log(transactions.length);
      
         cart.forEach( (item, i) => {
-            //console.log(`Transaction ${i}: ${appUser} + ${item}`);
+            console.log(`Transaction ${i}: ${appUser} + ${item}`);
             const transaction = {
                 user: appUser,
                 item: item,
             };
             
-            axios.post("/api/addTransaction", transaction);
+            axios.post("/api/addTransaction", transaction)
+                .then((res) => {
+                    if (res.data.success) {
+                        console.log("Transaction added!");
+                    } else {
+                        console.log("Failed to add transaction");
+                    }
+                })
+                .catch( () => "Error");
+
                 
                
         });
             
-    }   
+    };   
 
-   
+    
 
     return (
         <div className='App'>
@@ -80,7 +112,9 @@ const Store = ({appUser, setAppUser, items}) => {
                 </div>
                 <button onClick={handleSubmit}>Checkout</button>
             </div>
-            
+            <div>
+                <button onClick={fetchTransactions}>List Transactions</button>
+            </div>
         
         </div>
     )
